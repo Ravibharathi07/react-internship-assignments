53fdf6e4eeae22a8a92316cf72940785c4f2bc1f
Ravi bharathi R <74967563+Ravibharathi07@users.noreply.github.com>
2020-11-30 17:26:49 +0530

    Add files via upload

---
+++ loop1to100.js
@@ -0,0 +1,20 @@
+"use strict";
+
+
+
+
+
+    for(let i=1; i<=100;i++){
+        if(i%3===0 && i%5===0){
+            console.log("bestEnlistreact")
+        } else if(i%3==0){
+            console.log("bestEnlist")
+        }
+          else if (i%5==0){
+         console.log("react")
+       } 
+        else {
+            console.log(i)       
+         }
+    }
+
