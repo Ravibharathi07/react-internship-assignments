91795ef2a840ea06484e278df7c74c8d7cc1d250
Ravi bharathi R <74967563+Ravibharathi07@users.noreply.github.com>
2020-11-30 17:41:45 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Person from './Components/Person'
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hi i'm React !!!🎉</h1>
+      <Person/>
+      
+      
+
+    </div>
+  );
+}
+
+export default App;
